d0b7bf32eda0de89a94e2c28f43b3faa24f9c214
Султан <carameleclipsimus@gmail.com>
2023-04-17 08:31:08 +0600

    feat: third task of array is done

--- script.js
+++ script.js
@@ -19,4 +19,29 @@ doubleArray([1,2,3]) // [1,2,3,1,2,3]
 function doubleArray(arr) {
   return arr.concat(arr);
 }
-console.log(doubleArray([1, 2, 3]));
+// console.log(doubleArray([1, 2, 3]));
+
+// third
+/*
+Создать функцию, которая принимает произвольное (любое) число массивов и удаляет из каждого массива первый элемент,
+а возвращает массив из оставшихся значений.
+
+changeCollection([1,2,3], [‘a’, ’b’, ‘c’])  // [ [2,3], [‘b’, ‘c’] ]
+
+Не забудьте про проверкку того что массив является массивом.
+Это можно сделать с помощью метода Array.isArray(arr);
+*/
+function changeCollection() {
+  let result = [];
+  let value = "";
+  let variable = [];
+
+  for (let i = 0; i < arguments.length; i++) {
+    value = arguments[i].shift();
+    result.push(value);
+    console.log(arguments[i]);
+    variable.push(arguments[i]);
+  }
+  return variable;
+}
+// console.log(changeCollection([1, 2, 3], ["a", "b", "c"]));
