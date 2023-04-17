4ea5f92b0b60a2aec543ed8c9d1ac7f4728304e0
Султан <carameleclipsimus@gmail.com>
2023-04-17 19:18:27 +0600

    feat: fourth task of array is done...

--- script.js
+++ script.js
@@ -45,3 +45,53 @@ function changeCollection() {
   return variable;
 }
 // console.log(changeCollection([1, 2, 3], ["a", "b", "c"]));
+
+// fourth
+/*
+Создать функцию которая принимает массив пользователей,
+поле которое хочу проверить и значение на которое хочу проверять указанное поле.
+Проверять что все аргументы переданы. Если что то не переданно то возвращать объект ошибки return new Error('Error message').
+Возвращать новый массив с пользователями соответсвующие указанным параметрам.
+*/
+const users = [
+  {
+    _id: "5e36b779dc76fe3db02adc32",
+    balance: "$1,955.65",
+    picture: "http://placehold.it/32x32",
+    age: 33,
+    name: "Berg Zimmerman",
+    gender: "male",
+  },
+  {
+    _id: "5e36b779d117774176f90e0b",
+    balance: "$3,776.14",
+    picture: "http://placehold.it/32x32",
+    age: 37,
+    name: "Deann Winters",
+    gender: "female",
+  },
+  {
+    _id: "5e36b779daf6e455ec54cf45",
+    balance: "$3,424.84",
+    picture: "http://placehold.it/32x32",
+    age: 36,
+    name: "Kari Waters",
+    gender: "female",
+  },
+];
+function filterUsers(arr, key, value) {
+  let res = [];
+  let someVariable;
+  if (!arr || !key || !value) {
+    return new Error("Error message");
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][key] == value) {
+      someVariable = arr[i];
+      res.push(someVariable);
+    }
+  }
+  return res;
+}
+
+console.log(filterUsers(users, "age", 37));
